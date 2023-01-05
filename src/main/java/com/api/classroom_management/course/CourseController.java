5df5caab644b78f09dadf4b6653d13d4e5ef5856
thiag-off <thiagoforto20@gmail.com>
2023-01-04 23:35:54 -0300

    adicionado annotation @CrossOrigin para acesso do front-end

--- src/main/java/com/api/classroom_management/course/CourseController.java
+++ src/main/java/com/api/classroom_management/course/CourseController.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/course")
+@CrossOrigin(origins = "http://localhost:4200")
 public class CourseController {
 
     private final CourseService courseService;
